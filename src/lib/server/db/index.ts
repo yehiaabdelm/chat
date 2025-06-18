0b8a63f00ea8ae517f0e3769ce360262d697824f
yehiaabdelm <yehiaabdelm@gmail.com>
2025-06-18 14:55:30 +0300

    redeploy

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
+import { DATABASE_URL } from '$env/static/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(DATABASE_URL);
 
 export const db = drizzle(client, { schema, casing: 'snake_case' });
